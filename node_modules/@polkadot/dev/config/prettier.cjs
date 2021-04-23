ff111e795dbe3937e75074d8243f8bfbe11abc10
sbnair <shai567@live.com>
2021-04-24 01:22:39 +0530

    Check in changes done in klugdossier

---
+++ node_modules/@polkadot/dev/config/prettier.cjs
@@ -0,0 +1,11 @@
+// Copyright 2017-2021 @polkadot/dev authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+  jsxSingleQuote: true,
+  parser: 'babel-ts',
+  singleQuote: true,
+  tabWidth: 2,
+  // this conflicts with eslint, default override
+  trailingComma: 'none'
+};
---
+++ node_modules/@polkadot/react-qr/index.cjs
@@ -0,0 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "QrDisplayAddress", {
+  enumerable: true,
+  get: function () {
+    return _DisplayAddress.QrDisplayAddress;
+  }
+});
+Object.defineProperty(exports, "QrDisplayPayload", {
+  enumerable: true,
+  get: function () {
+    return _DisplayPayload.QrDisplayPayload;
+  }
+});
+Object.defineProperty(exports, "QrScanAddress", {
+  enumerable: true,
+  get: function () {
+    return _ScanAddress.QrScanAddress;
+  }
+});
+Object.defineProperty(exports, "QrScanSignature", {
+  enumerable: true,
+  get: function () {
+    return _ScanSignature.QrScanSignature;
+  }
+});
+Object.defineProperty(exports, "QrNetworkSpecs", {
+  enumerable: true,
+  get: function () {
+    return _NetworkSpecs.QrNetworkSpecs;
+  }
+});
+Object.defineProperty(exports, "packageInfo", {
+  enumerable: true,
+  get: function () {
+    return _packageInfo.packageInfo;
+  }
+});
+
+var _DisplayAddress = require("./DisplayAddress.cjs");
+
+var _DisplayPayload = require("./DisplayPayload.cjs");
+
+var _ScanAddress = require("./ScanAddress.cjs");
+
+var _ScanSignature = require("./ScanSignature.cjs");
+
+var _NetworkSpecs = require("./NetworkSpecs.cjs");
+
+var _packageInfo = require("./packageInfo.cjs");
